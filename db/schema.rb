65c796a602abcb201f19db93c55b392160a666ba
Jay Frey <jf.kuan@omesti.com>
2022-10-22 02:27:02 +0800

    Added foreign key user_id to urls table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_19_140453) do
+ActiveRecord::Schema.define(version: 2022_10_20_055438) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,6 +57,8 @@ ActiveRecord::Schema.define(version: 2021_05_19_140453) do
     t.text "url"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_urls_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -69,4 +71,5 @@ ActiveRecord::Schema.define(version: 2021_05_19_140453) do
 
   add_foreign_key "authentications", "users"
   add_foreign_key "statistics", "urls"
+  add_foreign_key "urls", "users"
 end
