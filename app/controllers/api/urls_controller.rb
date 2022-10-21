ab2dfeb8de9b3a7f0cbc2159299481186e5debf4
Jay Frey <jf.kuan@omesti.com>
2022-10-22 02:34:07 +0800

    Created APIs for unbind link and added checking for custom url

--- app/controllers/api/urls_controller.rb
+++ app/controllers/api/urls_controller.rb
@@ -1,5 +1,7 @@
 module Api
-  class UrlsController < ApiController
+  class UrlsController < AuthorizedApiController
+    skip_before_action :authorized?, except: [:unbind]
+
     # route only available for development and testing
     def new; end
 
@@ -9,7 +11,14 @@ module Api
 
       if !url
         slug = Url.generate_slug
-        url = Url.create(url: params[:url], slug: slug)
+
+        if params[:customUrl].blank?
+          slug = Url.generate_slug
+        else
+          slug = params[:customUrl]
+        end
+
+        url = Url.create(url: params[:url], slug: slug, user_id: params[:userId])
       end
 
       if url.valid?
@@ -25,6 +34,18 @@ module Api
 
       if url
         render json: { success: true, data: { url: url.as_json(methods: [:statistics]) } }
+      else
+        render json: { success: false }, status: :not_found
+      end
+    end
+
+    def unbind
+      url = Url.find_by(id: params[:url_id], user_id: current_user.id)
+    
+      if url
+        url.user_id = nil
+        url.save(validate: false)
+        render json: { success: true, data: { url: url } }
       else
         render json: { success: false }, status: :not_found
       end
